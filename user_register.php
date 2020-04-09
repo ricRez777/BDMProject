aefc236bf2b8ebde0f5cf1ae9c2a40ab7e5015db
ricrez777 <ricardo2397@hotmail.com>
2020-04-08 23:08:52 -0500

    Pantalla para registro de noticias

--- user_register.php
+++ user_register.php
@@ -23,17 +23,23 @@
                 <h1>Register</h1>
                 <label for="Name" class="formLabel">Name</label>
                 <input type="text" name="Name" placeholder="Name" class="formText">
+                
                 <label for="LastName" class="formLabel">Last Name</label>
                 <input type="text" name="LastName" placeholder="Last Name" class="formText">
+                
                 <label for="Email" class="formLabel">E-mail</label>
                 <input type="e-mail" name="Email" placeholder="Email" class="formText">
+                
                 <label for="UserName" class="formLabel">UserName</label>
                 <p>*Your username will be used as your signature*</p>
                 <input type="text" name="UserName" placeholder="User Name" class="formText">
+                
                 <label for="Pass" class="formLabel">Password</label>
                 <input type="password" name="Pass" placeholder="Password" class="formText">
+                
                 <label for="Photo" class="formLabel">Profile Picture</label>
                 <input type="file" name="Photo" class="formText">
+                
                 <input type="submit" value="Register now!" class="btn-Secondary">
             </div>
             <img src="img/register.jpg" width="500" height="400" alt="">
