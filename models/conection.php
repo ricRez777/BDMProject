eee2a10deaf3a5a8b50a121ba50a360d8b137cb0
Ricardo <ricardo2397@hotmail.com>
2020-04-19 12:15:02 -0500

    Conexion a la base de datos

---
+++ models/conection.php
@@ -0,0 +1,45 @@
+<?php 
+
+    class Conection{
+        private $Server;
+        private $Use;
+        private $NombreDB;
+        private $Pass;
+
+        public $cone;
+
+        function __construct(){
+            $this->Server = "localhost";
+            $this->Use = "root";
+            $this->Pass = "";
+            $this->NombreDB = "noticiasdb";
+        }
+
+        function conexion(){
+            try{
+                $this->cone = new mysqli($this->Server, $this->Use, $this->Pass, $this->NombreDB);
+                if ($this->cone->connect_errno) {
+                    echo "Failed to connect to MySQL: " . $this->cone->connect_error;
+                    exit();
+                  }
+                  else{
+                      echo "conection success";
+                  }
+            }
+            catch(Exception $e){
+                echo "ERROR";
+            }
+        }
+
+        function disconnect(){
+            $this->cone->close();
+            echo "conection close";
+        }
+
+    }
+
+    $varPrueba = new Conection();
+    $varPrueba->conexion();
+    
+    $varPrueba->disconnect();
+?>
